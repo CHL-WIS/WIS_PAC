3e6c2b98b5d572aaeef8c80c5ced67e3f252739a
Ty Hesser <thesser1@gmail.com>
2014-08-12 08:55:31 -0500

    updating

--- shells/run_post_nc.sh
+++ shells/run_post_nc.sh
@@ -34,12 +34,12 @@ cd $WORKDIR
 (   $SHEL/ww3_post_nc_point.sh  31 54 2 cali_l4  cali $STORM_NAME $BASE $BASIN $UNAME > ww3_post4.out    ) &
 (   $SHEL/ww3_post_nc_point.sh  ALL ALL 1 hawaii_l2 hawaii $STORM_NAME $BASE $BASIN $UNAME > ww3_post4.out    ) &
 (   $SHEL/ww3_post_nc_point.sh  ALL ALL 1 hawaii_l3 hawaii  $STORM_NAME $BASE $BASIN $UNAME > ww3_post4.out    ) &
-(   $SHEL/ww3_post_nc_field.sh  basin_l1 basin $STORM_NAME $BASE $BASIN $UNAME  > ww3_post1.out    ) &
-(   $SHEL/ww3_post_nc_field.sh  westc_l2 westc $STORM_NAME $BASE $BASIN $UNAME > ww3_post2.out    ) &
-(   $SHEL/ww3_post_nc_field.sh  westc_l3 westc $STORM_NAME $BASE $BASIN $UNAME > ww3_post3.out    ) &
-(   $SHEL/ww3_post_nc_field.sh  cali_l4  cali $STORM_NAME $BASE $BASIN $UNAME > ww3_post4.out    ) &
-(   $SHEL/ww3_post_nc_field.sh  hawaii_l2 hawaii $STORM_NAME $BASE $BASIN $UNAME > ww3_post4.out    ) &
-(   $SHEL/ww3_post_nc_field.sh  hawaii_l3 hawaii  $STORM_NAME $BASE $BASIN $UNAME > ww3_post4.out    ) &
+#(   $SHEL/ww3_post_nc_field.sh  basin_l1 basin $STORM_NAME $BASE $BASIN $UNAME  > ww3_post1.out    ) &
+#(   $SHEL/ww3_post_nc_field.sh  westc_l2 westc $STORM_NAME $BASE $BASIN $UNAME > ww3_post2.out    ) &
+#(   $SHEL/ww3_post_nc_field.sh  westc_l3 westc $STORM_NAME $BASE $BASIN $UNAME > ww3_post3.out    ) &
+#(   $SHEL/ww3_post_nc_field.sh  cali_l4  cali $STORM_NAME $BASE $BASIN $UNAME > ww3_post4.out    ) &
+#(   $SHEL/ww3_post_nc_field.sh  hawaii_l2 hawaii $STORM_NAME $BASE $BASIN $UNAME > ww3_post4.out    ) &
+#(   $SHEL/ww3_post_nc_field.sh  hawaii_l3 hawaii  $STORM_NAME $BASE $BASIN $UNAME > ww3_post4.out    ) &
 
 wait
 
@@ -64,10 +64,10 @@ cat > ${STORM_NAME}_post.sh << EOF
 #!/bin/bash
 #
 #PBS -N ${RUN_NAME}_post
-#PBS -q standard
+#PBS -q debug
 #PBS -A ERDCV03995SHS
 #PBS -l select=1:ncpus=32:mpiprocs=32
-#PBS -l walltime=01:30:00
+#PBS -l walltime=01:00:00
 #PBS -j oe
 #PBS -o ${RUN_NAME}_post.oe
 #PBS -m abe
@@ -84,11 +84,18 @@ cat > ${STORM_NAME}_post.sh << EOF
 # ---------------------------------
 #
 cd $WORKDIR
-
-aprun -n 1 -d 25 $WORKDIR/serial_post.sh > serial_post.out
+export OMP_NUM_THREADS=32
+#ccmrun  $WORKDIR/serial_post.sh -cpus=19 > serial_post.out 
+#( ccmrun $SHEL/ww3_post_nc_point.sh 1 30 1 basin_l1 basin $STORM_NAME $BASE $BASIN $UNAME -cpus=1 > ww3_post1.out )
+ ccmrun  $SHEL/ww3_post_nc_field.sh  basin_l1 basin $STORM_NAME $BASE $BASIN $UNAME  -cpus=32 > ww3_post1.out 
+ ccmrun   $SHEL/ww3_post_nc_field.sh  westc_l2 westc $STORM_NAME $BASE $BASIN $UNAME -cpus=32 > ww3_post2.out  
+ ccmrun   $SHEL/ww3_post_nc_field.sh  westc_l3 westc $STORM_NAME $BASE $BASIN $UNAME -cpus=32 > ww3_post3.out   
+ ccmrun   $SHEL/ww3_post_nc_field.sh  cali_l4  cali $STORM_NAME $BASE $BASIN $UNAME -cpus=32 > ww3_post4.out 
+ ccmrun   $SHEL/ww3_post_nc_field.sh  hawaii_l2 hawaii $STORM_NAME $BASE $BASIN $UNAME -cpus=32 > ww3_post4.out 
+ ccmrun  $SHEL/ww3_post_nc_field.sh  hawaii_l3 hawaii  $STORM_NAME $BASE $BASIN $UNAME -cpus=32 > ww3_post4.out  
 wait
 
-aprun -n 1 -d 6 $WORKDIR/tar_files.sh > tar_files.out
+#aprun -n 1 -d 6 $WORKDIR/tar_files.sh > tar_files.out
 wait
 #
 # ----------------------------------------------------------------
